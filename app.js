7dda014cec152f5bca28880977717fdae29fa9c2
MaryamJami1 <maryamchouhan373@gmail.com>
2024-08-21 01:33:05 +0500

    todo-list

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 let input = document.getElementById("input-value");
-function add(){
+function add() {
 
-let inputValue = input.value
-let li = document.createElement("li")
-li.innerHTML=inputValue
-let div = document.getElementById("li")
-div.appendChild(li).className="list"
+
+    if (input.value == "") {
+        alert("Please add your task")
+    }
+    else {
+        let inputValue = input.value
+        let li = document.createElement("li")
+        li.className = "list"
+        li.innerHTML = inputValue
+        let div = document.getElementById("li")
+        div.appendChild(li)
+        
+    }
+    input.value = "";
 }
